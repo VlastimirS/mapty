3443516b0ea655dd4a31bd353c1b5980a69de682
VlastimirS <vlastimir.samolov@gmail.com>
2022-08-08 23:26:10 +0300

    fix bugs

--- src/script.js
+++ src/script.js
@@ -1,13 +1,1 @@
 import "./sass/main.scss"
-
-const container = document.querySelectorAll(".observer")
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    entry.target.classList.toogle("show", entry.isIntersecting)
-  })
-})
-
-container.forEach((observer) => {
-  observer.observe()
-})
